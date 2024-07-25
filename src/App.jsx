f435ffb38b03dc842c49d3e441162524e8825613
Razan M. Fahmi <razan_assem@hotmail.com>
2024-07-25 14:13:32 +0300

    fix: readme.md

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Home from './Componants/Home';
 import Details from './Componants/Details';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Layout from './Componants/Layout';
-import Filter from './Componants/Filter';
 import BorderCountry from './Componants/BorderCountry';
 
 
@@ -16,7 +15,6 @@ export default function App() {
       path: "/rest-countries-api-with-color-theme-switcher-master/", element: <Layout />, children: [
         { index: true, element: <Home /> },
         { path: "/rest-countries-api-with-color-theme-switcher-master/home", element: <Home /> },
-        { path: "/rest-countries-api-with-color-theme-switcher-master/filter", element: <Filter /> },
         { path: "/rest-countries-api-with-color-theme-switcher-master/details/:country", element: <Details /> },
         { path: "/rest-countries-api-with-color-theme-switcher-master/border/:name", element: <BorderCountry /> },
 
